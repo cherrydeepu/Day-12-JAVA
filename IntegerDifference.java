6014bcf72e476ea128100eccd5f088ee5bd05865
Rishitha <trishithapandu2218@gmail.com>
2024-12-13 22:25:27 +0530

    Add files via upload

---
+++ IntegerDifference.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class IntegerDifference{
+    public static void main(String args[]){
+        int arr1[]={99,5,6,1,2,3};
+        int arr2[]={1,2,3,4,5,6};
+        Set<Integer> a=new HashSet();
+        Set<Integer> b=new HashSet();
+        for(int s1:arr1)
+        a.add(s1);
+        System.out.println(a);
+        for(int s2:arr2)
+        b.add(s2);
+        System.out.println(b);
+        Set<Integer> u=new HashSet<>();
+        u.removeAll(b);
+        System.out.println(u);
+    }
+}
