cb6804a82b909cd830b200da33887821ff66f195
Rishitha <trishithapandu2218@gmail.com>
2024-12-13 21:55:34 +0530

    Add files via upload

---
+++ IntegerUnion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class IntegerUnion{
+    public static void main(String args[]){
+        int arr1[]={99,5,6,1,2,3};
+        int arr2[]={1,2,3,4,5,6};
+        Set<Integer> a=new HashSet();
+        Set<Integer> b=new HashSet();
+        for(int s1:arr1)
+        a.add(s1);
+        System.out.println(a);
+        for(int s2:arr2)
+        b.add(s2);
+        System.out.println(b);
+    }
+}
